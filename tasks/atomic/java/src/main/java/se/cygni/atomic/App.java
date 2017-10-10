7dce09fdf35c07b9dc4d23480558b97cd3382386
Alexander Torstling <alexander@torstling.com>
2017-10-10 18:33:34 +0200

    atomic: non-threaded rust version

--- tasks/atomic/java/src/main/java/se/cygni/atomic/App.java
+++ tasks/atomic/java/src/main/java/se/cygni/atomic/App.java
@@ -6,17 +6,17 @@ public class App {
 
     static int i = 0;
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+    public static void main(String[] args) throws Exception {
         final int nThreads = 10;
 
         final CountDownLatch allStarted = new CountDownLatch(nThreads);
-        final CountDownLatch mayBegin = new CountDownLatch(1);
+        final CountDownLatch startSignal = new CountDownLatch(1);
         final CountDownLatch done = new CountDownLatch(nThreads);
         for (int j = 0; j < nThreads; j++) {
-            new Thread(() -> {
+            final Thread t = new Thread(() -> {
                 try {
                     allStarted.countDown();
-                    mayBegin.await(1, TimeUnit.SECONDS);
+                    await(startSignal, "Timeout waiting for start signal");
                     for (int j1 = 0; j1 < 1000; j1++) {
                         i += 1;
                     }
@@ -24,14 +24,19 @@ public class App {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            }).start();
+            });
+            t.start();
         }
-        allStarted.await(1, TimeUnit.SECONDS);
-        mayBegin.countDown();
-        final boolean success = done.await(5, TimeUnit.SECONDS);
-        if (!success) {
-            throw new RuntimeException("Timeout while waiting for threads to terminate");
-        }
+        await(allStarted, "Timeout waiting for threads to start");
+        startSignal.countDown();
+        await(done, "Timeout while waiting for threads to terminate");
         System.out.println("Result of 10*1000 additions: " + i);
+    }
+
+    private static void await(CountDownLatch latch, String message) throws InterruptedException {
+        final boolean success = latch.await(1, TimeUnit.SECONDS);
+        if (!success) {
+            throw new RuntimeException(message);
+        }
     }
 }
--- tasks/atomic/rust/src/main.rs
+++ tasks/atomic/rust/src/main.rs
@@ -1,3 +1,10 @@
+extern crate crossbeam;
+
 fn main() {
-    println!("Hello, world!");
+   let mut i = 0;
+   for j in 1..10001 {
+       i+=1;
+   }
+   
+   println!("Result of 10*1000 increments: {}", i);
 }
