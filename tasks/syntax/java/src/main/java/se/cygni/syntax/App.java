5f565356de241525c9c8a9160b7c3f6f5b6ca316
Martin Barksten <martin.barksten@gmail.com>
2017-10-15 17:20:00 +0200

    Add some simple tests to show how the syntax works

---
+++ tasks/syntax/java/src/main/java/se/cygni/syntax/App.java
@@ -0,0 +1,13 @@
+package se.cygni.syntax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
