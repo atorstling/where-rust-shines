b0b953becdd32a35068f3381737a9cf23e677f4e
Alexander Torstling <alexander@torstling.com>
2017-10-16 19:10:04 +0200

    atomic: user version

--- tasks/atomic/rust/src/main.rs
+++ tasks/atomic/rust/src/main.rs
@@ -1,20 +1,20 @@
 extern crate crossbeam;
-use std::sync::atomic::{AtomicUsize, Ordering};
 
 fn main() {
     let thread_count = 10;
     let increments_per_thread = 100000;
-   let i = AtomicUsize::new(0);
-   crossbeam::scope(|scope| {
+    let i = 0;
+    crossbeam::scope(|scope| {
         for _ in 0..thread_count {
             scope.spawn(|| {
-                for _ in 0..increments_per_thread {
-                    //i+-=1;
-                    i.fetch_add(1, Ordering::Release);
-                }
-            });
+                            for _ in 0..increments_per_thread {
+                                //i+=1;
+                            }
+                        });
         }
     });
-   println!("Result of {}*{} increments: {}", thread_count, increments_per_thread, i.into_inner());
-   //println!("Result of {}*{} increments: {}", thread_count, increments_per_thread, i.load(Ordering::Acquire));
+    println!("Result of {}*{} increments: {}",
+             thread_count,
+             increments_per_thread,
+             i);
 }
